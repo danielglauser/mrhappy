48ab3aed4119b7bca91b41b59ea5e0fb0b36192d
Daniel Glauser <daniel@snugghome.com>
2013-01-22 14:33:11 -0500

    Added support files for sentimental and first working version of analyzed-email route

--- project.clj
+++ project.clj
@@ -4,7 +4,8 @@
   :dependencies [[org.clojure/clojure "1.4.0"]
                  [compojure "1.1.5"]
                  [org.clojars.gnarmis/sentimental "0.1.1-SNAPSHOT"]]
-  :plugins [[lein-ring "0.8.2"]]
+  :plugins [[lein-ring "0.8.2"]
+            [lein-swank "1.4.4"]]
   :ring {:handler mrhappy.handler/app}
   :profiles
   {:dev {:dependencies [[ring-mock "0.1.3"]]}})
--- src/mrhappy/handler.clj
+++ src/mrhappy/handler.clj
@@ -1,15 +1,44 @@
 (ns mrhappy.handler
   (:use [compojure.core]
         [ring.middleware.resource]
-        [ring.middleware.file])
+        [ring.middleware.file]
+        [sentimental.core])
   (:require [compojure.handler :as handler]
             [compojure.route :as route]))
 
+(defn analyze [text]
+  (println (str "Contents: " text))
+  (categorize text))
+
+(defn convert-to-percentage
+  "Takes in one of the following forms and maps it to a percentage of happiness:
+strongsubj-positive 100%
+weaksubj-positive    80%
+strongsubj-neutral   60%
+weaksubj-neutral     40%
+weaksubj-negative    20%
+strongsubj-negative   0%
+"
+  [str]
+  (cond
+    (= "strongsubj-positive" str) 100
+    (= "weaksubj-positive") 80
+    (= "strongsubj-neutral") 60
+    (= "weaksubj-neutral") 40
+    (= "weaksubj-negative") 20
+    (= "strongsubj-negative") 0))
+
 (defn analyze-email []
-  "bam!")
+  (let [dir (clojure.java.io/file "data/email")
+        files (file-seq dir)]
+    (println (str "Found " (count files) " file(s)"))
+    (map #(-> %
+              slurp
+              analyze
+              convert-to-percentage)
+         (rest files))))
 
 (defroutes app-routes
-  (GET "/" [] "Hello World")
   (GET "/analyzed-email" [] (analyze-email))
   (route/not-found "Not Found"))
 
